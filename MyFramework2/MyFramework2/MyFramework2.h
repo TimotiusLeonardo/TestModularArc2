c77070eacef5718b030d53797ae184a1706cf062
Timotius Leonardo <60493932+TimotiusLeonardo@users.noreply.github.com>
2022-07-07 15:53:53 +0700

    Create FrameworkModule

---
+++ MyFramework2/MyFramework2/MyFramework2.h
@@ -0,0 +1,18 @@
+//
+//  MyFramework2.h
+//  MyFramework2
+//
+//  Created by Timotius Leonardo Lianoto on 07/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyFramework2.
+FOUNDATION_EXPORT double MyFramework2VersionNumber;
+
+//! Project version string for MyFramework2.
+FOUNDATION_EXPORT const unsigned char MyFramework2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyFramework2/PublicHeader.h>
+
+
